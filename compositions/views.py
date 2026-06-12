a0c489611cc24d610050e5678e3a855fd803a13a
SpxcxJLJ <jefterjeanlouis@gmail.com>
2026-06-12 09:40:30 -0400

    Initial Django project

--- compositions/views.py
+++ compositions/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Composition, CompositionDetail
+from .serializers import CompositionSerializer, CompositionDetailSerializer
+
+class CompositionViewSet(viewsets.ModelViewSet):
+    queryset = Composition.objects.all()
+    serializer_class = CompositionSerializer
+
+class CompositionDetailViewSet(viewsets.ModelViewSet):
+    queryset = CompositionDetail.objects.all()
+    serializer_class = CompositionDetailSerializer
+
